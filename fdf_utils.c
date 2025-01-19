d2a26b27a978e9f34706156193bc53edd47dd9e7
Axedassis <lssvgabriel@gmail.com>
2025-01-18 23:55:01 -0300

    Feature: Functionality to read rows and columns

--- fdf_utils.c
+++ fdf_utils.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 15:54:55 by lsilva-x          #+#    #+#             */
-/*   Updated: 2025/01/17 20:45:18 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/01/18 23:52:30 by lsilva-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,19 +31,83 @@ int	check_path(const char *f_name)
 		i--;
 	if (i == -1 || !ft_strcmp((f_name + 1), ".fdf"))
 		error_exit(ERR_INVALID_FILE_FORMAT, 5);
-	return (0);
+	return (1);
 }
 
-int	set_dimensions(t_map *map, const char *f_name)
+char	*read_map(int fd)
+{
+	char	*str;
+	char	*str_tmp;
+
+	str_tmp = get_next_line(fd);
+	if (!str_tmp)
+		error_exit(ERR_GNL, 7);
+	while (1)
+	{
+		if (str_tmp != 0)
+			str = ft_strjoin(str, str_tmp);
+		else
+			break;
+		free (str_tmp);
+		str_tmp = get_next_line(fd);
+	}
+	close (fd);
+	return (str);
+}
+
+int	get_rows(const char *str)
+{
+	int		rows;
+	int		i;
+
+	rows = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n' || str[i + 1] == 0)
+			rows++;
+		i++;
+	}
+	ft_printf("[Log]: Rows file: %d\n", rows);
+	return (rows);
+}
+
+int	get_columns(const char *str)
+{
+	int		columns;
+	int		i;
+
+	i = 0;
+	columns = 0;
+	while(str[i] != '\n')
+	{
+		if (str[i] != ' ' && i == 0)
+			columns++;
+		else if(str[i - 1] == ' ' && str[i] != ' ')
+			columns++;
+		i++;
+	}
+	ft_printf("[Log]: Columns file: %d\n", columns);
+	return (columns);
+}
+
+void	get_metadata(t_map *map, const char *str)
+{
+	map->rows = get_rows(str);
+	map->columns = get_columns(str);
+	
+}
+
+int	init_map(t_mlx *mlx, const char *f_name)
 {
 	int		fd;
-	int		width;
-	int		heigth;
+	char	*str;
 
 	fd = open(f_name, O_RDONLY);
 	if (!fd)
 		return (0);
-	
+	str = read_map(fd);
+	get_metadata(&mlx->data_map, str);
 }
 
 t_mlx	*init_fdf(const char *f_name)
@@ -55,7 +119,7 @@ t_mlx	*init_fdf(const char *f_name)
 		error_exit(ERR_MALLOC, 2);
 	if (!check_path(f_name))
 		error_exit(ERR_INVALID_PATH, 3);
-	if (!set_dimensions(mlx->map, f_name))
+	if (!init_map(mlx, f_name))
 		error_exit(ERR_INVALID_PATH, 6);
 		
 	return (mlx);
--- macros.h
+++ macros.h
@@ -6,15 +6,13 @@
 /*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 16:46:48 by lsilva-x          #+#    #+#             */
-/*   Updated: 2025/01/17 20:43:30 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/01/18 23:10:39 by lsilva-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #ifndef MACROS_H
 #define MACROS_H
-
-#include "./fdf.h"
 
 /*
                                        _      
@@ -30,6 +28,7 @@
 #define ERR_INVALID_PATH "[Error]: The provided path is invalid"
 #define ERR_EMPTY_PATH "[Error]: The provided path is empty"
 #define ERR_INVALID_FILE_FORMAT "[Error]: The provided file format is invalid"
+#define ERR_GNL "[Error]: GNL had an error"
 
 /*
      _                   _   
@@ -53,15 +52,30 @@ typedef struct s_img
 
 typedef struct s_map
 {
-	
+	int		size;
+	int		rows;
+	int		columns;
+	int		mult;
+	int		origin_x;
+	int		origin_y;
 }	t_map;
+
+typedef struct s_point
+{
+	int		x;
+	int		y;
+	int		z;
+	int		color;
+}	t_point;
+
 
 typedef struct s_mlx
 {
 	void	*mlx;
 	void	*win;
 	t_img	img;
-	t_map	map;
+	t_map	data_map;
+	t_point	*pts;
 }	t_mlx;
 
 #endif
